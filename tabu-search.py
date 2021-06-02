a832adfbaeb2a47266c4ebe509ee140bf4cb8f5e
Eddy-123 <adegnandjoueddy12@gmail.com>
2021-06-02 22:15:44 +0100

    TS: initial random solution + step 1 with SwapMove as neighbor generator

--- tabu-search.py
+++ tabu-search.py
@@ -33,10 +33,27 @@ solution_2 = [2, 3, 5, 10, 6, 8, 9, 4, 7, 1]
 fitness_1 = Objfun(instance_dict, solution_1, show = True)
 fitness_2 = Objfun(instance_dict, solution_2, show = True)
 
-print("fitness 1 = ", fitness_1)
+def get_InitialSolution(instance_dict, seed, show = False):
+	n_jobs = len(instance_dict)
+	initial_solution = list(range(1, n_jobs + 1))
+	rd.seed(seed)
+	rd.shuffle(initial_solution)
+	if show == True:
+		print("initial Random Solution: {}".format(initial_solution))
+	return initial_solution
 
 
+def SwapMove(solution, i, j):
+	solution = solution.copy()
+	
+	i_index = solution.index(i)
+	j_index = solution.index(j)
+	
+	solution[i_index], solution[j_index] = solution[j_index], solution[i_index]
+	return solution
 
+initial_solution = get_InitialSolution(instance_dict, 2021, show = True)
+print('Neighbor solution: {}'.format(SwapMove(initial_solution, 10, 5)))
 
 
 
